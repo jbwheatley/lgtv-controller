9598ac1a8c101e5447ae54c0255fa4118f9ed76d
Jack Wheatley <jackbwheatley@gmail.com>
2020-08-17 13:40:41 +0100

    remove config deps

--- src/main/scala/lgtv/LGTVConfig.scala
+++ src/main/scala/lgtv/LGTVConfig.scala
@@ -1,32 +1,7 @@
 package lgtv
 
-import java.io.File
-
-import cats.effect.IO
-import com.typesafe.config.ConfigFactory.{defaultReference, parseFileAnySyntax, parseResourcesAnySyntax}
-import com.typesafe.config.{Config, ConfigFactory}
-import net.ceedubs.ficus.Ficus.{stringValueReader, toFicusConfig}
-import net.ceedubs.ficus.readers.ArbitraryTypeReader.arbitraryTypeValueReader
-
 final case class LGTVConfig(tvIp: String, wolIp: String, macAddress: String, keycode: String)
 
 object LGTVConfig {
-  def load: IO[LGTVConfig] =
-    loadDefaultTypesafeConfig.flatMap(conf => IO(conf.as[LGTVConfig]("lgtv")))
-
-  private def loadDefaultTypesafeConfig: IO[Config] =
-    IO {
-      scala.util.Properties
-        .propOrNone("config.file")
-        .map(config => parseFileAnySyntax(new File(config)))
-        .getOrElse(ConfigFactory.empty())
-    }.flatMap(loadTypesafeConfig)
-
-  private def loadTypesafeConfig(baseConfig: Config): IO[Config] =
-    IO {
-      baseConfig
-        .withFallback(parseResourcesAnySyntax("application"))
-        .withFallback(defaultReference())
-        .resolve()
-    }
+  def apply(): LGTVConfig = LGTVConfig(Secrets.tvIp, Secrets.wolIp, Secrets.macAddress, Secrets.keycode)
 }
