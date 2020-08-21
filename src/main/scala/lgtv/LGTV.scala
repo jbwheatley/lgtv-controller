93ee4bbf796c2f8210aa68129fa108b9a1f6c2da
jbwheatley <19939655+jbwheatley@users.noreply.github.com>
2020-08-21 23:45:41 +0100

    use fs2.io for managing sockets

--- src/main/scala/lgtv/LGTV.scala
+++ src/main/scala/lgtv/LGTV.scala
@@ -1,32 +1,34 @@
 package lgtv
 
-import java.net.{DatagramPacket, DatagramSocket, InetSocketAddress, Socket}
+import java.net.InetSocketAddress
 
-import cats.effect.IO
+import cats.effect.{Concurrent, ContextShift, IO, Resource}
 import cats.implicits.{catsKernelStdMonoidForList, catsSyntaxSemigroup}
+import fs2.Chunk
+import fs2.io.udp.Packet
+import fs2.io.tcp
+import fs2.io.udp
 
-class LGTV(encryption: Encryption)(config: LGTVConfig) {
+class LGTV(
+    encryption: Encryption,
+    config: LGTVConfig,
+    udpSocketGroup: Resource[IO, udp.SocketGroup],
+    tcpSocketGroup: Resource[IO, tcp.SocketGroup]
+)(implicit CS: ContextShift[IO], Con: Concurrent[IO]) {
   private val magicPacket: Array[Byte] = {
     val m = config.macAddress.split(':').toList.map("#" + _).map(Integer.decode).map(_.byteValue()).combineN(16)
     (List.fill(6)(Byte.MaxValue) ++ m).toArray
   }
 
   def wake(): IO[Unit] =
-    for {
-      socket <- IO(new DatagramSocket())
-      _ <- IO(
-        socket.send(
-          new DatagramPacket(magicPacket, magicPacket.length, new InetSocketAddress(config.wolIp, 9))
-        )
-      )
-      _ <- IO(socket.close())
-    } yield ()
+    udpSocketGroup
+      .flatMap(_.open[IO]())
+      .use(_.write(Packet(new InetSocketAddress(config.wolIp, 9), Chunk.bytes(magicPacket))))
 
   def sendCommand(command: Command): IO[Unit] =
-    for {
-      socket <- IO(new Socket(config.tvIp, 9761))
-      out    <- IO(socket.getOutputStream)
-      _      <- encryption.encrypt(command).map(cEnc => out.write(cEnc.message))
-      _      <- IO(out.close())
-    } yield ()
+    tcpSocketGroup.flatMap(_.client(new InetSocketAddress(config.tvIp, 9761))).use { socket =>
+      encryption.encrypt(command).flatMap { cEnc =>
+        socket.write(Chunk.bytes(cEnc.message))
+      }
+    }
 }
