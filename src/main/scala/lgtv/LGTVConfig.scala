85b0b16281fe3eac42f3f2d0f34ab08f3b574b50
Jack Wheatley <jackbwheatley@gmail.com>
2020-08-22 16:53:33 +0100

    unused imports

--- src/main/scala/lgtv/LGTVConfig.scala
+++ src/main/scala/lgtv/LGTVConfig.scala
@@ -3,5 +3,6 @@ package lgtv
 final case class LGTVConfig(tvIp: String, wolIp: String, macAddress: String, keycode: String)
 
 object LGTVConfig {
+  //Just loading from an object because too lazy to deal with config files
   def apply(): LGTVConfig = LGTVConfig(Secrets.tvIp, Secrets.wolIp, Secrets.macAddress, Secrets.keycode)
 }
