4adaa1250593ccffa7de7ab8b21f26a91f75e60f
jbwheatley <19939655+jbwheatley@users.noreply.github.com>
2021-04-28 16:17:29 +0100

    fix LGTV

--- src/main/scala/lgtv/LGTV.scala
+++ src/main/scala/lgtv/LGTV.scala
@@ -3,7 +3,7 @@ package lgtv
 import cats.effect.IO
 import com.comcast.ip4s.{IpLiteralSyntax, SocketAddress}
 import fs2.Chunk
-import fs2.io.net.{Datagram, Network}
+import fs2.io.net.{Datagram, Network, SocketOption}
 
 class LGTV(
     encryption: Encryption,
@@ -15,16 +15,19 @@ class LGTV(
     Chunk.array(Array.fill(6)(Byte.MaxValue) ++ Array.fill(16)(m).flatten)
   }
 
+  private val udpOptions: List[SocketOption] = SocketOption.broadcast(true) :: Nil
+
   def wake(): IO[Unit] =
     network
-      .openDatagramSocket()
-      .use(_ => IO(println(config.wolIp.toInetAddress)))
-//      .use(_.write(Datagram(SocketAddress(config.wolIp, port"9"), magicPacket)))
+      .openDatagramSocket(options = udpOptions)
+      .use(_.write(Datagram(SocketAddress(config.wolIp, port"9"), magicPacket)))
 
   def sendCommand(command: Command): IO[Unit] =
-    network.client(SocketAddress(config.wolIp, port"9761")).use { socket =>
-      encryption.encrypt(command).flatMap { cEnc =>
-        socket.write(Chunk.array(cEnc.message))
+    network
+      .client(SocketAddress(config.tvIp, port"9761"))
+      .use { socket =>
+        encryption.encrypt(command).flatMap { cEnc =>
+          socket.write(Chunk.array(cEnc.message))
+        }
       }
-    }
 }
